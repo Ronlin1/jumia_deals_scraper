28ddd91f462c60e0ebc2a15a1da0d07fbec4bad3
Ronlin1 <ronlinx6@gmail.com>
2021-12-11 10:09:20 +0300

    changes to jumia deals

--- jumia_deals.py
+++ jumia_deals.py
@@ -1,25 +1,20 @@
-import os
 import pandas as pd
 from bs4 import BeautifulSoup as beauty
 import requests
 
-url = 'https://deals.jumia.ug/real-estate'
-
-# since Jumia deals - real estate has 6472 pages of data
+url = "https://deals.jumia.ug/real-estate"
 all_urls = []
-for page in range(1, 6473):
-    next_urls = url + '?page=' + str(page)
-    all_urls.append(next_urls)
-    # print(next_urls)
 
-# print(all_urls)
+for page in range(1,9500):
+    next_urls = url + "?page=" + str(page)
+    all_urls.append(next_urls)
+
 for url in all_urls:
     render = requests.get(url)
-    # print(render)
-    the_html = beauty(render.content, 'html.parser')
+    the_html = beauty(render.content, "html.parser")
     # print(the_html)
 
-    scrape = the_html.find_all(class_="text-area")
+    scrape =the_html.find_all(class_ = "text-area")
     # print(scrape)
 
     scraped_data = []
@@ -27,12 +22,11 @@ for url in all_urls:
         scraped_data.append(data.get_text())
         # print(data.get_text())
     # print(scraped_data)
-    clean_data = [data.replace('\n', '') for data in scraped_data]
-    clean_data_ = [data.replace('   ', '') for data in clean_data]
+    cleaned_data = [data.replace("\n", "") for data in scraped_data]
+    # print(cleaned_data)
+    clean_data_ = [data.replace("   ", "") for data in cleaned_data]
     # print(clean_data_)
 
-    data_2_csv = pd.DataFrame(clean_data, columns=['column'])
-    data_2_csv.to_csv('jumia_deals', index=False)
+    data_2_csv = pd.DataFrame(clean_data_, columns=["column"])
+    data_2_csv.to_csv("jumia.csv", mode="a", index=False)
     print(data_2_csv)
-
-    """ Print statements are for testing..."""
